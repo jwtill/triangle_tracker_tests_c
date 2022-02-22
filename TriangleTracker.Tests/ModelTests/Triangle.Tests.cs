a878aedf7aeee6d8aabea4530a3d820edd5ee1a9
Josh Tillinghast <jwtill@icloud.com>
2022-02-22 10:31:41 -0800

    Add rest of files

---
+++ TriangleTracker.Tests/ModelTests/Triangle.Tests.cs
@@ -0,0 +1,14 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using ProjectName;
+
+namespace TriangleTracker.Tests
+{
+  [TestClass]
+  public class TriangleTests
+  {
+    [TestMethod]
+    public void IsTriangle_HasThreeSides_True()
+    {
+      Assert.AreEqual(EXPECTED RESULT, CODE TO TEST);
+}
+  }
