5066f8701cae29c37d1b5e112379603f5bbbcc8e
Josh Tillinghast <jwtill@icloud.com>
2022-02-22 16:17:41 -0800

    Built tests, add UI
    Co-authored-by: marisa-edgar <marisa-edgar@users.noreply.github.com>

--- TriangleTracker.Tests/ModelTests/Triangle.Tests.cs
+++ TriangleTracker.Tests/ModelTests/Triangle.Tests.cs
@@ -1,14 +1,58 @@
 using Microsoft.VisualStudio.TestTools.UnitTesting;
-using ProjectName;
+using Shapes;
 
-namespace TriangleTracker.Tests
+namespace Shapes.Tests
 {
   [TestClass]
   public class TriangleTests
   {
     [TestMethod]
-    public void IsTriangle_HasThreeSides_True()
+    public void IsTriangle_HasThreeSides_False()
     {
-      Assert.AreEqual(EXPECTED RESULT, CODE TO TEST);
-}
+      TriangleTracker testTriangle = new TriangleTracker();
+      Assert.AreEqual("Not a triangle", testTriangle.TriangleType(0,1,1));
+    }
+
+    [TestMethod]
+    public void IsTriangle_Side1Largest_True()
+    {
+      TriangleTracker testTriangle = new TriangleTracker();
+      Assert.AreEqual("Not a triangle", testTriangle.TriangleType(5,1,1));
+    }
+
+    [TestMethod]
+    public void IsTriangle_Side2Largest_True()
+    {
+      TriangleTracker testTriangle = new TriangleTracker();
+      Assert.AreEqual("Not a triangle", testTriangle.TriangleType(1,5,1));
+    }
+
+    [TestMethod]
+    public void IsTriangle_Side3Largest_True()
+    {
+      TriangleTracker testTriangle = new TriangleTracker();
+      Assert.AreEqual("Not a triangle", testTriangle.TriangleType(1,1,5));
+    }
+
+    [TestMethod]
+    public void IsTriangle_IsEquilateral_Equilateral()
+    {
+      TriangleTracker testTriangle = new TriangleTracker();
+      Assert.AreEqual("This is an equilateral triangle", testTriangle.TriangleType(5,5,5));
+    }
+
+    [TestMethod]
+    public void IsTriangle_IsIsosceles_IsIsosceles()
+    {
+      TriangleTracker testTriangle = new TriangleTracker();
+      Assert.AreEqual("This is an isosceles triangle", testTriangle.TriangleType(5,5,8));
+    }
+
+    [TestMethod]
+    public void IsTriangle_IsScalene_IsScalene()
+    {
+      TriangleTracker testTriangle = new TriangleTracker();
+      Assert.AreEqual("This is an scalene triangle", testTriangle.TriangleType(5,6,8));
+    }
   }
+}
